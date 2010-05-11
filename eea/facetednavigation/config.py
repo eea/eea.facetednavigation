eabb24eaf66879d33caf88fb6a2c738a5de4ab7a
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-05-11 14:51:29 +0000

    #3277 Refactored field.Vocabulary to be reused also with other fields than StringField.
    git-svn-id: https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.facetednavigation/trunk@16498 24821446-e7ea-0310-81ac-b189f4507b0b

--- eea/facetednavigation/config.py
+++ eea/facetednavigation/config.py
@@ -4,3 +4,9 @@ ANNO_CRITERIA = 'FacetedCriteria'
 ANNO_FACETED_LAYOUTS = 'FacetedLayouts'
 ANNO_FACETED_LAYOUT = 'FacetedLayout'
 ANNO_FACETED_VERSION = 'FacetedVersion'
+try:
+    import zope.annotation
+except ImportError:
+    PLONE = 2.5
+else:
+    PLONE = 3
--- eea/facetednavigation/widgets/field.py
+++ eea/facetednavigation/widgets/field.py
@@ -2,24 +2,31 @@
 """
 from zope import component
 from zope.app.schema.vocabulary import IVocabularyFactory
-from Products.Archetypes.Field import StringField as ArchStringField
-from Products.Archetypes.utils import DisplayList
+from Products.Archetypes import atapi
+from eea.facetednavigation.config import PLONE
 
-class StringField(ArchStringField):
+class FieldMixin(object):
     """ Override Vocabulary method to accept zope3 like vocabularies
     """
     def Vocabulary(self, content_instance=None):
         """ Override Vocabulary method (copied from plone 3)
         """
+        if PLONE >= 3:
+            return super(FieldMixin, self).Vocabulary(content_instance)
+
         factory_name = getattr(self, 'vocabulary_factory', None)
         if not factory_name:
-            return ArchStringField.Vocabulary(self, content_instance)
+            return super(FieldMixin, self).Vocabulary(content_instance)
 
         factory = component.getUtility(IVocabularyFactory, name=factory_name)
         factory_context = content_instance
         if factory_context is None:
             factory_context = self
-        self.vocabulary = DisplayList([(t.value, t.title or t.token)
+        self.vocabulary = atapi.DisplayList([(t.value, t.title or t.token)
                                        for t in factory(factory_context)])
 
-        return ArchStringField.Vocabulary(self, content_instance)
+        return super(FieldMixin, self).Vocabulary(content_instance)
+
+class StringField(FieldMixin, atapi.StringField):
+    """ Plone3 like string field
+    """
