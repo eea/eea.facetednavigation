6e2543994cb4a39ce3954cffb2abd2ec40ac2892
avoinea <avoinea@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-10-30 12:46:04 +0000

    - Cleanup plone2.5 code  - Added collective.js.jqueryui dependency.
    git-svn-id: http://svn.plone.org/svn/collective/eea.facetednavigation/trunk@225553 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- eea/facetednavigation/setuphandlers.py
+++ eea/facetednavigation/setuphandlers.py
@@ -1,11 +1,7 @@
 """ Various setup
 """
-from plone25 import setupVarious as plone_setup
-
 def setupVarious(context):
     """ Do some various setup.
     """
     if context.readDataFile('eeafacetednavigation.txt') is None:
         return
-
-    plone_setup(context)
