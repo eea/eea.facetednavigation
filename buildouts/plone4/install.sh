e2e56e7862f7e2de2d1fbc2595a28f338015d6b1
David Batranu <dbatranu@gmail.com>
2013-01-21 11:15:51 +0200

    Normalizing buildout (Refs #9567)

--- buildouts/plone4/install.sh
+++ buildouts/plone4/install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-if [ ! -f bootstrap.sh ]
-then
-    wget -O bootstrap.sh https://svn.eionet.europa.eu/repositories/Zope/trunk/www.eea.europa.eu/trunk/install.sh
-fi
+wget -O bootstrap.sh https://svn.eionet.europa.eu/repositories/Zope/trunk/www.eea.europa.eu/trunk/install.sh
 chmod u+x bootstrap.sh
 ./bootstrap.sh
