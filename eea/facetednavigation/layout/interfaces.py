57fdd068de685ebcf3e94f8a37d6bb62003353ed
tdesvenain <thomas.desvenain@gmail.com>
2013-04-22 10:07:07 +0200

    document IFacetedLayout interface

--- eea/facetednavigation/layout/interfaces.py
+++ eea/facetednavigation/layout/interfaces.py
@@ -9,3 +9,15 @@ class ILayoutMenuHandler(interface.Interface):
 class IFacetedLayout(interface.Interface):
     """ Utility to get available layouts, current layout.
     """
+
+    layout = interface.Attribute("""Current layout""")
+    layouts = interface.Attribute("""Available layouts""")
+    default_layouts = interface.Attribute("""et container default layouts""")
+
+    def get_macro(self, template_id=None, macro='content-core'):
+        """ Get macro from layout
+        """
+
+    def update_layout(self, layout=None):
+        """ Save layout for context. Returns error if any.
+        """
