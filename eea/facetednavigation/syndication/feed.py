293c19ab76f51b32fba7c5a256827bbded833f27
David Batranu <dbatranu@gmail.com>
2013-07-17 15:43:36 +0300

    RSS2 feed generation in Plone 4.3 (Refs #14323)
    - removing Products.fatsyndication support
    - removing bda.feed support
    - providing correct query for Plone 4.3 based syndication

--- eea/facetednavigation/syndication/feed.py
+++ eea/facetednavigation/syndication/feed.py
@@ -1,53 +1,16 @@
 """ Feed
 """
-from zope import interface
 from zope import component
+from Products.CMFPlone.browser.syndication.adapters import FolderFeed
 
-from Products.basesyndication import interfaces as baseinterfaces
-from Products.fatsyndication import adapters as fatadapters
 
-from eea.facetednavigation.interfaces import IFacetedNavigable
-try:
-    from bda.feed import interfaces as bdainterfaces
-    ILogo = bdainterfaces.ILogo
-except ImportError:
-    class ILogo(interface.Interface):
-        """ No logo """
-
-class FacetedFeedSource(fatadapters.BaseFeedSource):
-    """ Feed source
+class FacetedFeed(FolderFeed):
+    """ Feed
     """
-    interface.implements(baseinterfaces.IFeedSource)
-    component.adapts(IFacetedNavigable)
-
-    def getFeedEntries(self):
-        """ Feed entries
-        """
+    def _brains(self):
         request = getattr(self.context, 'REQUEST',
                           getattr(self.context, 'request', None))
         handler = component.getMultiAdapter((self.context, request),
                                             name=u'faceted_query')
 
-        brains = handler.query(ajax=False)
-        for brain in brains:
-            doc = brain.getObject()
-            if not doc:
-                continue
-            adapter = component.queryAdapter(doc, baseinterfaces.IFeedEntry)
-            if not adapter:
-                continue
-            yield adapter
-
-class FacetedFeed(fatadapters.BaseFeed):
-    """ Feed
-    """
-    interface.implements(baseinterfaces.IFeed)
-    component.adapts(IFacetedNavigable)
-
-    def getImageURL(self):
-        """ Feed logo
-        """
-        logo = component.queryAdapter(self.context, ILogo)
-        if logo:
-            return logo()
-        return ''
+        return handler.query(ajax=False)
