e222392ff0752cfbc87dca97ac015ce7aaf6a4f8
Alin Voinea <alin@eaudeweb.ro>
2016-06-03 18:12:11 +0300

    WIP - Add zope schema for faceted widgets

--- eea/facetednavigation/widgets/interfaces.py
+++ eea/facetednavigation/widgets/interfaces.py
@@ -1,5 +1,6 @@
 """ Widgets interfaces
 """
+from zope import schema
 from zope.interface import Interface
 from zope.configuration.fields import GlobalObject
 from zope.i18nmessageid import ZopeMessageFactory as _
@@ -33,4 +34,33 @@ class IWidgetDirective(Interface):
                       " implementation object.  This must identify an"
                       " object in a module using the full dotted name."),
         required=True,
+    )
+
+class ICommonEditSchema(Interface):
+    """ Common edit schema for Faceted Widgets
+    """
+    # Schemata default
+    title = schema.TextLine(
+        title=_(u"Friendly name"),
+        description=_(u"Title for widget to display in view page")
+    )
+
+    # Schemata layout
+    position = schema.Choice(
+        title=_(u'Position'),
+        description=_(u"Widget position in page"),
+        vocabulary="eea.faceted.vocabularies.WidgetPositions"
+    )
+
+    # schemata="layout",
+    section = schema.Choice(
+        title=_(u"Section"),
+        description=_("Display widget in section"),
+        vocabulary="eea.faceted.vocabularies.WidgetSections",
+    )
+
+    # schemata="layout",
+    hidden = schema.Bool(
+        title=_(u'Hidden'),
+        description=_(u"Hide widget"),
     )
