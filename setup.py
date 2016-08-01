1c9c6d2570061763b7ccd0c0ab593c4e08826365
Alin Voinea <alin@eaudeweb.ro>
2016-08-01 09:48:28 +0300

    Refs #104 - Update fixed eea.jquery version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name=NAME,
           # -*- Extra requirements: -*-
           'eea.faceted.vocabularies > 5.9',
           'collective.js.jqueryui',
-          'eea.jquery > 9.0',
+          'eea.jquery > 9.1',
       ],
       extras_require={
           'test': [
