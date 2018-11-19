2311b39a80348c3bb726100ab7ae3d1cedbfbb38
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-11-19 18:58:38 +0200

    Jenkins - Fix tests

--- eea/facetednavigation/widgets/widget.py
+++ eea/facetednavigation/widgets/widget.py
@@ -32,6 +32,10 @@ logger = logging.getLogger('eea.facetednavigation')
 def compare(a, b):
     """ Compare lower values
     """
+    if not isinstance(a, unicode):
+        a = a.decode('utf-8')
+    if not isinstance(b, unicode):
+        b = b.decode('utf-8')
     return cmp(a.lower(), b.lower())
 
 #
