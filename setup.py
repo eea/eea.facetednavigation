62f465eb556f6562658d963cda9b2a52146e7d0d
Alin Voinea <contact@avoinea.com>
2022-07-21 18:53:43 +0300

    Black setup.py

--- setup.py
+++ setup.py
@@ -4,56 +4,59 @@ import os
 from os.path import join
 from setuptools import setup, find_packages
 
-NAME = 'eea.facetednavigation'
-PATH = NAME.split('.') + ['version.txt']
+NAME = "eea.facetednavigation"
+PATH = NAME.split(".") + ["version.txt"]
 VERSION = open(join(*PATH)).read().strip()
 
-setup(name=NAME,
-      version=VERSION,
-      description="EEA Faceted Navigation",
-      long_description_content_type="text/x-rst",
-      long_description=(
-          open("README.rst").read() + "\n" + open(os.path.join("docs", "HISTORY.txt")).read()
-      ),
-      # https://pypi.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Framework :: Plone",
-          "Framework :: Plone :: 5.2",
-          "Framework :: Plone :: 6.0",
-          "Framework :: Plone :: Addon",
-          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='EEA Faceted Navigation FacetedNavigation Add-ons Plone Zope',
-      author='European Environment Agency: IDM2 A-Team',
-      author_email='eea-edw-a-team-alerts@googlegroups.com',
-      url='https://github.com/eea/eea.facetednavigation',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['eea'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'collective.js.jqueryui',
-          'eea.jquery > 10.2',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'plone.testing',
-              'plone.app.contenttypes',
-              'zope.testrunner',
-          ],
-      },
-      entry_points="""
+setup(
+    name=NAME,
+    version=VERSION,
+    description="EEA Faceted Navigation",
+    long_description_content_type="text/x-rst",
+    long_description=(
+        open("README.rst").read()
+        + "\n"
+        + open(os.path.join("docs", "HISTORY.txt")).read()
+    ),
+    # https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Addon",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="EEA Faceted Navigation FacetedNavigation Add-ons Plone Zope",
+    author="European Environment Agency: IDM2 A-Team",
+    author_email="eea-edw-a-team-alerts@googlegroups.com",
+    url="https://github.com/eea/eea.facetednavigation",
+    license="GPL version 2",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["eea"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        # -*- Extra requirements: -*-
+        "collective.js.jqueryui",
+        "eea.jquery > 10.2",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.testing",
+            "plone.app.contenttypes",
+            "zope.testrunner",
+        ],
+    },
+    entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
-      """
-      )
+      """,
+)
