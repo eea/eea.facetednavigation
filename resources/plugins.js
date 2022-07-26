c82e2884269ea6fa606bbacf1da47123adc0efc2
Alin Voinea <contact@avoinea.com>
2022-07-26 21:03:00 +0300

    Cleanup: Use jquery bbq from npmjs

--- resources/plugins.js
+++ resources/plugins.js
@@ -1,6 +1,7 @@
+import 'jquery2-bbq';
+
 import "./third-party/cookie/jquery.cookie.js";
 import "./third-party/ajaxfileupload/jquery.ajaxfileupload.js";
-import "./third-party/bbq/jquery.ba-bbq.min.js";
 import "./third-party/jstree/jquery.tree.js";
 import "./third-party/select2uislider/selectToUISlider.jQuery.js";
 import "./third-party/tagcloud/jquery.tagcloud.js";
