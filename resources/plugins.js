724ec2f2c36d25c053530405641dc042410d9782
Alin Voinea <contact@avoinea.com>
2022-07-27 17:50:46 +0300

    Cleanup: use jquery.cookie from npm

--- resources/plugins.js
+++ resources/plugins.js
@@ -1,6 +1,6 @@
 import 'jquery2-bbq';
+import 'jquery.cookie';
 
-import "./third-party/cookie/jquery.cookie.js";
 import "./third-party/ajaxfileupload/jquery.ajaxfileupload.js";
 import "./third-party/jstree/jquery.tree.js";
 import "./third-party/select2uislider/selectToUISlider.jQuery.js";
